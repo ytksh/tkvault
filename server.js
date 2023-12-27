787c5479204ec6667fc245923c7e869898eea9ba
tksh <119751708+ytksh@users.noreply.github.com>
2023-12-27 20:54:45 -0300

    refactors most of the code to meet standards

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
+/**
+ * This is a server configuration file for an Express application.
+ * It includes configurations for file uploads using Multer, rate limiting using express-rate-limit, 
+ * and some utility functions.
+ */
+
 const express = require('express');
 const multer = require('multer');
 const fs = require('fs');
 const path = require('path');
-const rateLimit = require('express-rate-limit'); 
+const rateLimit = require('express-rate-limit');
 const { generatePassword } = require('./scripts/utils');
 
 const app = express();
+const PORT = 3000;
+
+// Middleware
 app.set('view engine', 'ejs');
 app.use(express.json());
 app.use(express.static('public'));
@@ -14,30 +23,25 @@ app.set('trust proxy', 1);
 
 // Multer configuration
 const storage = multer.diskStorage({
-  destination: function(req, file, cb) {
-    cb(null, 'uploads/')
-  },
-  filename: function(req, file, cb) {
-    cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname))
-  }
-})
-
-const upload = multer({ storage: storage });
+  destination: (req, file, cb) => cb(null, 'uploads/'),
+  filename: (req, file, cb) => cb(null, `${file.fieldname}-${Date.now()}${path.extname(file.originalname)}`)
+});
+const upload = multer({ storage });
 
 // Rate limiter
 const uploadLimiter = rateLimit({
   windowMs: 2 * 60 * 1000, // 2 minutes
-  max: 1, // limit each IP to 1 requests per windowMs
-  message: "Too many uploads from this IP, please try again after 2 minutes"
+  max: 1, // limit each IP to 1 request per windowMs
+  message: 'Too many uploads from this IP, please try again after 2 minutes'
 });
 
+// Utility function to censor part of an IP address
 function censorIp(ip) {
-  // Remove the ::ffff: prefix if it exists
   if (ip.startsWith('::ffff:')) {
     ip = ip.substring(7);
   }
 
-  let parts = ip.split('.');
+  const parts = ip.split('.');
   if (parts.length === 4) {
     parts[2] = parts[3] = 'xxx';
   }
@@ -45,64 +49,56 @@ function censorIp(ip) {
 }
 
 // Routes
+
+// Home route
 app.get('/', (req, res) => {
   let uploadData = [];
   try {
-    if (fs.existsSync('uploadData.json')) {
-      uploadData = JSON.parse(fs.readFileSync('uploadData.json'));
-    } else {
-      fs.writeFileSync('uploadData.json', JSON.stringify([]));
-    }
+    uploadData = fs.existsSync('uploadData.json') ? JSON.parse(fs.readFileSync('uploadData.json')) : [];
   } catch (error) {
     console.error('Error reading or writing uploadData.json:', error);
   }
   res.render('index', { files: uploadData });
 });
 
+// Upload route
 app.post('/upload', uploadLimiter, upload.single('file'), async (req, res) => {
-  let ip = req.headers['x-forwarded-for'] || req.ip;
-  ip = ip.split(',')[0];
+  const clientIp = req.headers['x-forwarded-for'] || req.ip;
   const uploadTime = new Date();
-
-  // Generate a random password
   const password = generatePassword(8);
 
-  // Read existing upload data
   let uploadData = [];
   try {
-    if (fs.existsSync('uploadData.json')) {
-      uploadData = JSON.parse(fs.readFileSync('uploadData.json'));
-    }
+    uploadData = fs.existsSync('uploadData.json') ? JSON.parse(fs.readFileSync('uploadData.json')) : [];
   } catch (error) {
     console.error('Error reading uploadData.json:', error);
   }
 
-  // Add new upload data
-  console.log(`--NEW UPLOAD-- User IP: ${req.ip}\n Data: ${uploadTime} \nFilename: ${req.file.filename}\nPassword: ${password}`);
-  uploadData.push({
+  const newUpload = {
     fileName: req.file.filename,
-    uploaderIP: censorIp(ip),
-    uploadTime: uploadTime,
+    uploaderIP: censorIp(clientIp),
+    uploadTime,
     fileSize: req.file.size,
-    password: password
-  });
+    password
+  };
 
-  // Write updated upload data back to file
+  console.log(`--NEW UPLOAD-- User IP: ${clientIp}\n Data: ${uploadTime}\nFilename: ${newUpload.fileName}\nPassword: ${password}`);
+  uploadData.push(newUpload);
+
   try {
     fs.writeFileSync('uploadData.json', JSON.stringify(uploadData));
   } catch (error) {
     console.error('Error writing uploadData.json:', error);
   }
 
-  // Send the password to the client
-  res.json({ password: password });
+  res.json({ password });
 });
 
+// Delete route
 app.delete('/delete/:fileName', (req, res) => {
   const fileName = req.params.fileName;
   const password = req.body.password;
 
-  // Read existing upload data
   let uploadData = [];
   try {
     uploadData = JSON.parse(fs.readFileSync('uploadData.json'));
@@ -110,24 +106,18 @@ app.delete('/delete/:fileName', (req, res) => {
     console.error('Error reading uploadData.json:', error);
   }
 
-  // Find the file with the given name
   const fileIndex = uploadData.findIndex(file => file.fileName === fileName);
   if (fileIndex === -1) {
     return res.status(404).json({ error: 'File not found' });
   }
 
-  // Check the password
   if (uploadData[fileIndex].password !== password) {
     return res.status(403).json({ error: 'Incorrect password' });
   }
 
-  // Delete the file
   fs.unlinkSync(path.join('uploads', fileName));
-
-  // Remove the file's data
   uploadData.splice(fileIndex, 1);
 
-  // Write updated upload data back to file
   try {
     fs.writeFileSync('uploadData.json', JSON.stringify(uploadData));
   } catch (error) {
@@ -137,6 +127,7 @@ app.delete('/delete/:fileName', (req, res) => {
   res.json({ message: 'File deleted' });
 });
 
-app.listen(3000, () => {
-  console.log('Server started on port 3000');
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
 });
