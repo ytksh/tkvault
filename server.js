8b8c07f988c8b06e3010933e5ded500f198c773d
tksh <119751708+ytksh@users.noreply.github.com>
2023-12-27 17:30:51 -0300

    hotfix ip

--- server.js
+++ server.js
@@ -49,7 +49,8 @@ app.get('/', (req, res) => {
 });
 
 app.post('/upload', uploadLimiter, upload.single('file'), async (req, res) => {
-  const ip = req.ip;
+  let ip = req.headers['x-forwarded-for'] || req.ip;
+  ip = ip.split(',')[0];
   const uploadTime = new Date();
 
   // Generate a random password
