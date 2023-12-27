b5baceeff3c9323fcb301d92560a393432c119f5
tksh <119751708+ytksh@users.noreply.github.com>
2023-12-27 16:57:24 -0300

    more stuff

--- server.js
+++ server.js
@@ -1,42 +1,25 @@
-// Required modules
-const fs = require('fs');
-const path = require('path'); 
 const express = require('express');
 const multer = require('multer');
-const rateLimit = require('express-rate-limit');
+const fs = require('fs');
+const path = require('path');
+const rateLimit = require('express-rate-limit'); 
+const { generatePassword } = require('./scripts/utils');
 
-// Express app
 const app = express();
+app.set('view engine', 'ejs');
+app.use(express.json());
+app.use(express.static('public'));
+app.use('/uploads', express.static('uploads'));
 
-// Define rate limit rule
+const upload = multer({ dest: 'uploads/' });
+
+// Rate limiter
 const uploadLimiter = rateLimit({
   windowMs: 2 * 60 * 1000, // 2 minutes
   max: 1, // limit each IP to 1 requests per windowMs
   message: "Too many uploads from this IP, please try again after 2 minutes"
 });
 
-// Multer storage configuration
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, 'public/uploads/')
-  },
-  filename: (req, file, cb) => {
-    cb(null, Date.now() + path.extname(file.originalname)) // append the original file extension
-  }
-});
-
-// Middleware
-app.use('/uploads', express.static('uploads')); // Serve static files from the "uploads" directory
-app.use(express.static('public')); // Serve static files from the "public" directory
-const upload = multer({ storage: storage });
-
-// View engine
-app.set('view engine', 'ejs');
-
-// Port
-const port = process.env.PORT || 3000;
-
-// Function to censor IP
 function censorIp(ip) {
   // Remove the ::ffff: prefix if it exists
   if (ip.startsWith('::ffff:')) {
@@ -69,10 +52,15 @@ app.post('/upload', uploadLimiter, upload.single('file'), async (req, res) => {
   const ip = req.ip;
   const uploadTime = new Date();
 
+  // Generate a random password
+  const password = generatePassword(8);
+
   // Read existing upload data
   let uploadData = [];
   try {
-    uploadData = JSON.parse(fs.readFileSync('uploadData.json'));
+    if (fs.existsSync('uploadData.json')) {
+      uploadData = JSON.parse(fs.readFileSync('uploadData.json'));
+    }
   } catch (error) {
     console.error('Error reading uploadData.json:', error);
   }
@@ -82,7 +70,8 @@ app.post('/upload', uploadLimiter, upload.single('file'), async (req, res) => {
     fileName: req.file.filename,
     uploaderIP: censorIp(ip),
     uploadTime: uploadTime,
-    fileSize: req.file.size // Add this line
+    fileSize: req.file.size,
+    password: password
   });
 
   // Write updated upload data back to file
@@ -92,10 +81,49 @@ app.post('/upload', uploadLimiter, upload.single('file'), async (req, res) => {
     console.error('Error writing uploadData.json:', error);
   }
 
-  res.redirect('/');
+  // Send the password to the client
+  res.json({ password: password });
 });
 
-// Start server
-app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
+app.delete('/delete/:fileName', (req, res) => {
+  const fileName = req.params.fileName;
+  const password = req.body.password;
+
+  // Read existing upload data
+  let uploadData = [];
+  try {
+    uploadData = JSON.parse(fs.readFileSync('uploadData.json'));
+  } catch (error) {
+    console.error('Error reading uploadData.json:', error);
+  }
+
+  // Find the file with the given name
+  const fileIndex = uploadData.findIndex(file => file.fileName === fileName);
+  if (fileIndex === -1) {
+    return res.status(404).json({ error: 'File not found' });
+  }
+
+  // Check the password
+  if (uploadData[fileIndex].password !== password) {
+    return res.status(403).json({ error: 'Incorrect password' });
+  }
+
+  // Delete the file
+  fs.unlinkSync(path.join('uploads', fileName));
+
+  // Remove the file's data
+  uploadData.splice(fileIndex, 1);
+
+  // Write updated upload data back to file
+  try {
+    fs.writeFileSync('uploadData.json', JSON.stringify(uploadData));
+  } catch (error) {
+    console.error('Error writing uploadData.json:', error);
+  }
+
+  res.json({ message: 'File deleted' });
+});
+
+app.listen(3000, () => {
+  console.log('Server started on port 3000');
 });
